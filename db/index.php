d247f8b05f35f6a8d7abdbab7a4443636a27a133
MorilloKovach <xxsmating@gmail.com>
2025-07-11 22:51:07 -0300

    Eliminar ciertos parametros

--- db/index.php
+++ db/index.php
@@ -1,12 +1,4 @@
 <?php
-
-/*
-Tenemos que definir como tomar la base de datos dentro de nuestro programa. Dado a que necesitamos centralizar la informacion, entonces vamos a tomar de exclusiva los valores exactos que requerimos, y por el momento, no nos va a importar mucho el como trabajemos con estos.
-
-*/
-
-//creame la conexion con mi db de localhost
-
 $conexion = mysqli_connect("localhost", "root", "", "orientatec");
 if (!$conexion) {
     die("Connection failed: " . mysqli_connect_error());
